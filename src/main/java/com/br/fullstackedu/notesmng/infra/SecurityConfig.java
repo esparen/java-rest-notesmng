5cfdd97da3d85b5e8a7947c5253639b62208ec83
Eder Sparenberger <edsparen@gmail.com>
2024-04-24 15:28:06 -0300

    feat(security): adds project initial security settings

---
+++ src/main/java/com/br/fullstackedu/notesmng/infra/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.br.fullstackedu.notesmng.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) // Excessão de Token no Ponto de Entrada -> endpoint
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())); // Excessão de Token Negado
+        return http.build();
+    }
+}
