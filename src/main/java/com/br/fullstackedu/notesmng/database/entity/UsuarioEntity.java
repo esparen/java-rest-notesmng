935ed65ccb54af8511643696d16ceb14b3bd0ef7
Ederson Sparenberger <72260390+esparen@users.noreply.github.com>
2024-04-24 14:45:22 -0300

    feat(database): adds project entities and repositories (#3)

---
+++ src/main/java/com/br/fullstackedu/notesmng/database/entity/UsuarioEntity.java
@@ -0,0 +1,69 @@
+package com.br.fullstackedu.notesmng.database.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "aluno")
+public class UsuarioEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotNull(message = "Atributo nome_usuario é obrigatório")
+    private String nomeUsuario;
+
+    private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "usuarios_papeis",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "perfil_id")
+    )
+    private Set<PerfilEntity> perfis;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.getPerfis();
+    }
+
+    @Override
+    public String getPassword() {
+        return getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return getNomeUsuario();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/br/fullstackedu/notesmng/database/repository/CadernoRepository.java
@@ -0,0 +1,9 @@
+package com.br.fullstackedu.notesmng.database.repository;
+
+
+import com.br.fullstackedu.notesmng.database.entity.CadernoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CadernoRepository extends JpaRepository<CadernoEntity, Long> {
+
+}
