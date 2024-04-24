8c3959c0642397cea73f16b84777a9afd4ce1c9e
Ederson Sparenberger <72260390+esparen@users.noreply.github.com>
2024-04-24 14:26:45 -0300

    Feature: Project Init (#2)
    * feat(app): adds project initial structure
    
    * feat(database): adds PostgreSQL database connection

---
+++ src/main/java/com/br/fullstackedu/notesmng/NotesmngApplication.java
@@ -0,0 +1,13 @@
+package com.br.fullstackedu.notesmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesmngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesmngApplication.class, args);
+	}
+
+}
