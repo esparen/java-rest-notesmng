b8376cedd83dd1398dd544e940ae439f437d500f
Eder Sparenberger <edsparen@gmail.com>
2024-04-24 17:56:12 -0300

    feat(login): adds login endpoint

--- src/main/java/com/br/fullstackedu/notesmng/database/repository/UsuarioRepository.java
+++ src/main/java/com/br/fullstackedu/notesmng/database/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package com.br.fullstackedu.notesmng.database.repository;
 import com.br.fullstackedu.notesmng.database.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
+    Optional<UsuarioEntity> findByNomeUsuario(String nomeUsuario);
 }
